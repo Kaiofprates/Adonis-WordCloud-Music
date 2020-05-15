662d5d769b50317cbdd4cec3184ab39b9774172b
kaiofprates <kaioprates@hotmail.com>
2020-05-15 01:14:56 -0300

    first commit

---
+++ app/Controllers/Http/CloudController.js
@@ -0,0 +1,6 @@
+'use strict'
+
+class CloudController {
+}
+
+module.exports = CloudController
---
+++ app/Controllers/Http/MainController.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const axios = require('axios')
+
+class MainController {
+    index({ view }) {
+
+    return view.render('cloud', {
+        data: 'Ola mundico!'
+    })
+    }
+     async store({request, view}){
+         const music = request.input('music')
+         const artist = request.input('artist')
+         const key  = process.env.VAGALUME_API
+         let letra = ''
+         if ( music && artist){
+            await axios.get("https://api.vagalume.com.br/search.php"
+            + "?art=" + artist
+            + "&mus=" + music,
+            + `&apikey=${key}`).then((response) => { 
+                if(response){
+                     try{
+                      letra = response.data.mus[0].text
+                     }catch(err){
+                         console.log(err)
+                     }
+                }else{
+                    letra = "Music not found!"
+                }
+               })
+         }
+         return view.render('cloud', {
+             data: letra
+         })
+     }
+}
+
+module.exports = MainController
